347184b755fde491c78724164dd71512913f3609
diacris <38329014+diacris@users.noreply.github.com>
2018-07-12 14:58:40 +0300

    Add project files.

---
+++ ProblemaAlgoritmica/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemaAlgoritmica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti ora:");
+
+            var ora = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Introduceti minutele:");
+
+            var minute = Convert.ToInt32(Console.ReadLine());
+
+
+            if (minute == 0)
+            {
+                string traducereOra = TraducereOra(ora);
+                Console.WriteLine($"este {traducereOra} fix ");
+            }
+            if (minute <= 30 && minute > 0)
+            {
+                Console.WriteLine(" {ora} si 30 min dupa ");
+            }
+            if (minute > 30)
+            {
+                Console.WriteLine(" {ora} si 30 min fara ");
+            }
+
+
+            Console.ReadKey();
+        }
+
+        public static string TraducereOra(int ore)
+        {
+            string traducere;
+            switch (ore)
+            {
+                case 1:
+                    traducere = "ora unu";
+                    break;
+                case 2:
+                    traducere = "ora doi";
+                    break;
+                default:
+                    traducere = "ora necunoscuta";
+                    break;
+
+            }
+            return traducere;
+        }
+    }   
+}
